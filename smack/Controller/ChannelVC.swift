065150e2f0b58211761f7ddea80e5ed0e97a7bb9
Nicolas TERRONE <nicolasterrone@gmail.com>
2019-02-09 09:57:36 +0100

    Implemented SwReveal slide menu and organized MVC

---
+++ smack/Controller/ChannelVC.swift
@@ -0,0 +1,22 @@
+//
+//  ChannelVC.swift
+//  smack
+//
+//  Created by Nicolas Terrone on 09/02/2019.
+//  Copyright © 2019 Nicolas Terrone. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+    }
+ 
+
+}
---
+++ smack/Controller/ChatVC.swift
@@ -0,0 +1,28 @@
+//
+//  ChatVC.swift
+//  smack
+//
+//  Created by Nicolas Terrone on 09/02/2019.
+//  Copyright © 2019 Nicolas Terrone. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    //OUTLETS
+    
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+        
+    }
+    
+
+}
