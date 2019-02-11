b2ff1b3c626d2561539d220e5115bea0bc87780a
Nicolas TERRONE <nicolasterrone@gmail.com>
2019-02-11 13:21:28 +0100

    Added LoginVC, UI and Segues

---
+++ smack/Utilities/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  smack
+//
+//  Created by Nicolas Terrone on 11/02/2019.
+//  Copyright © 2019 Nicolas Terrone. All rights reserved.
+//
+
+import Foundation
+
+let TO_LOGIN = "toLogin"
