7c213256567e772ecacd2e4903aae39e3297313a
Nicolas TERRONE <nicolasterrone@gmail.com>
2019-02-16 11:11:29 +0100

    Added AuthService and prepared registerUser WebRequest

--- smack/Utilities/Constants.swift
+++ smack/Utilities/Constants.swift
@@ -8,6 +8,20 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+//USER_DEFAULT
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
+
+//URL
+let BASE_URL = "https://chattyappter.herokuapp.com/"
+let REGISTER_URL = "\(BASE_URL)account/register"
+
+
+// Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
