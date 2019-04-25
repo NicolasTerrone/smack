6ad9b1e978948a27d994fc067856b5b7745ff526
Nicolas TERRONE <nicolasterrone@gmail.com>
2019-04-25 13:09:25 +0200

    Added EditProfileVC

---
+++ smack/Controller/EditProfileVC.swift
@@ -0,0 +1,46 @@
+//
+//  EditProfileVC.swift
+//  smack
+//
+//  Created by Nicolas Terrone on 25/04/2019.
+//  Copyright © 2019 Nicolas Terrone. All rights reserved.
+//
+
+import UIKit
+
+class EditProfileVC: UIViewController {
+    
+    //OUTLETS
+    @IBOutlet weak var userImg: UIImageView!
+    @IBOutlet weak var userNameLbl: UITextField!
+    @IBOutlet weak var userEmail: UILabel!
+    @IBOutlet weak var bgView: UIView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+    
+    func setupView() {
+        userImg.image =  UIImage(named: UserDataService.instance.avatarName)
+        userImg.backgroundColor = UserDataService.instance.returnUIColor(components: UserDataService.instance.avatarColor)
+        userNameLbl.text = UserDataService.instance.name
+        userEmail.text = UserDataService.instance.email
+        let closeTouch = UITapGestureRecognizer(target: self, action: #selector(EditProfileVC.handTap))
+        bgView.addGestureRecognizer(closeTouch)
+    }
+
+    @objc func handTap() {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    //ACTIONS
+    @IBAction func closeBtnPressed(_ sender: Any){
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func validateBtnPressed(_ sender: Any){
+        
+    }
+}
--- smack/Services/AuthService.swift
+++ smack/Services/AuthService.swift
@@ -157,4 +157,6 @@ class AuthService {
     }
     
     
+    
+    
 }
